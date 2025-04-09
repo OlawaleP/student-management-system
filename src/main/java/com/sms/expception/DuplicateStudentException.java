d3bdc2c076f6bf1676f3b7ec251e415119da2bfb
OlawaleP <olawaleojo7j@gmail.com>
2025-04-09 14:20:53 +0100

    feat: duplicate student exception

---
+++ src/main/java/com/sms/expception/DuplicateStudentException.java
@@ -0,0 +1,11 @@
+package main.java.com.sms.expception;
+
+public package com.sms.exception;
+
+public class DuplicateStudentException extends Exception {
+    public DuplicateStudentException(String message) {
+        super(message);
+    }
+} {
+  
+}
